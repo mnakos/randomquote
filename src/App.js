292db1abe74e8fd0afa93c55cdafa3a6a298442d
Marianna Nakos <143140451+mnakos@users.noreply.github.com>
2023-12-31 13:14:00 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import RandomQuote from './Components/RandomQuote/RandomQuote';
+
+function App() {
+  return (
+    <div>
+      <RandomQuote/>
+    </div>
+  );
+}
+
+export default App;
